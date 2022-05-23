3bbe8d02ae0e22b5caf295e0edadd28efe432ada
Toru Kobayashi <koba0004@gmail.com>
2022-05-23 22:55:52 +0900

    feat: redirect with normalized amazon URL

--- src/lib.rs
+++ src/lib.rs
@@ -32,9 +32,15 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
         .get("/", |req, _| {
             let re = Regex::new(r"/dp/\w*?/").unwrap();
             let url = &req.url().unwrap().to_string();
-            let caps = re.captures(url).unwrap();
-            let amazon_url = format!("https://amazon.co.jp/{}", caps.get(0).unwrap().as_str());
-            Response::from_html(format!("<html><body><h1>Amazon URL shorter</h1><p>Location: {}</p></body></html>", amazon_url))
+            console_log!("url is {}", url);
+            if let Some(caps) = re.captures(url) {
+                let amazon_url = format!("https://amazon.co.jp/{}", caps.get(0).unwrap().as_str());
+                // Response::from_html(format!("<html><body><h1>Amazon URL shorter</h1><p>Location: {}</p></body></html>", amazon_url))
+                Response::redirect(Url::parse(&amazon_url).unwrap())
+            } else {
+                // Response::from_html("<html><body><h1>Amazon URL Shorter</h1><form action=\"/\"><input type=value name=q placeholder\"string like /?https://amazon.co.jp/***/dp/{id}/***</\" /></form></body></html>")
+                Response::ok("test")
+            }
         })
         .get("/worker-version", |_, ctx| {
             let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
