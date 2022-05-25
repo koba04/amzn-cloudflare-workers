dbda58748318f82f80e28d439a0a19ba96a29564
Toru Kobayashi <koba0004@gmail.com>
2022-05-26 00:25:51 +0900

    feat: implement amazon shorter URLs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use worker::*;
+use urlencoding::decode;
 
 mod utils;
 
@@ -12,6 +13,26 @@ fn log_request(req: &Request) {
         req.cf().region().unwrap_or("unknown region".into())
     );
 }
+
+fn shorten(url: String) -> Option<String> {
+    let re = Regex::new(r"/dp/\w*?/").unwrap();
+    if let Some(caps) = re.captures(&url) {
+        format!("https://amazon.co.jp/{}", caps.get(0).unwrap().as_str())
+    } else {
+        None
+    }
+}
+
+static HTML: &str = r#"
+<html>
+    <body>
+        <h1>Amazon URL Shorter</h1>
+        <form method=get action=/shorten>
+            <input style=width:400 type=value name=q placeholder=https://amazon.co.jp/***/dp/{id}/*** />
+        </form>
+    </body>
+</html
+"#;
 
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
@@ -29,17 +50,21 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // functionality and a `RouteContext` which you can use to  and get route parameters and
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
     router
-        .get("/", |req, _| {
-            let re = Regex::new(r"/dp/\w*?/").unwrap();
-            let url = &req.url().unwrap().to_string();
-            console_log!("url is {}", url);
-            if let Some(caps) = re.captures(url) {
-                let amazon_url = format!("https://amazon.co.jp/{}", caps.get(0).unwrap().as_str());
-                // Response::from_html(format!("<html><body><h1>Amazon URL shorter</h1><p>Location: {}</p></body></html>", amazon_url))
+        .get("/", move |req, _| {
+            if let Some(amazon_url) = shorten(req.url().unwrap().to_string()) {
                 Response::redirect(Url::parse(&amazon_url).unwrap())
             } else {
-                // Response::from_html("<html><body><h1>Amazon URL Shorter</h1><form action=\"/\"><input type=value name=q placeholder\"string like /?https://amazon.co.jp/***/dp/{id}/***</\" /></form></body></html>")
-                Response::ok("test")
+                Response::from_html(HTML)
+                // Response::ok("test")
+            }
+        })
+        .get("/shorten", |req, _| {
+            let url = decode(&req.url().unwrap().to_string()).expect("UTF-8").into_owned();
+            if let Some(amazon_url) = shorten(url) {
+                Response::redirect(Url::parse(&amazon_url).unwrap())
+            } else {
+                Response::from_html(HTML)
+                // Response::ok("test")
             }
         })
         .get("/worker-version", |_, ctx| {
